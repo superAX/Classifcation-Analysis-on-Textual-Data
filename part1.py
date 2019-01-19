f0f3245cacdcc4c06ba417d108b7b764d5bf47b0
Zaurbek Tsorojev <tsorojev.zaurbek@gmail.com>
2019-01-18 20:06:01 -0800

    Add question 4 (first part)

--- part1.py
+++ part1.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction import text
+import nltk
+nltk.download('wordnet')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from string import punctuation
+import numpy as np
+from sklearn.datasets import fetch_20newsgroups
+
 """
 QUESTION 1
 """
 print("----------- QUESTION 1 -----------")
 from matplotlib import pyplot as plt
-import numpy as np
-from sklearn.datasets import fetch_20newsgroups
-
 
 newsgroups_train = fetch_20newsgroups(subset='train')
 dictt = {}
@@ -35,43 +47,115 @@ plt.grid(True)
 plt.show()
 """
 
+#%%
 """
 QUESTION 2
 """
 print("----------- QUESTION 2 -----------")
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction import text
 
-categories = ['comp.graphics', 'comp.os.ms-windows.misc',
-'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
-'rec.autos', 'rec.motorcycles',
-'rec.sport.baseball', 'rec.sport.hockey']
+categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 
+              'comp.sys.mac.hardware', 'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 
+              'rec.sport.hockey']
 train_dataset = fetch_20newsgroups(subset = 'train', categories = categories, 
                                    shuffle = True, random_state = None)
 test_dataset = fetch_20newsgroups(subset = 'test', categories = categories, 
                                   shuffle = True, random_state = None)
 
+stop_words_skt = text.ENGLISH_STOP_WORDS
+stop_words_en = stopwords.words('english')
+combined_stopwords = set.union(set(stop_words_en),set(punctuation),set(stop_words_skt))
 
+#  Converts Penn Treebank tags to WordNet.
+def penn2morphy(penntag):
+    morphy_tag = {'NN':'n', 'JJ':'a',
+                  'VB':'v', 'RB':'r'}
+    try:
+        return morphy_tag[penntag[:2]]
+    except:
+        return 'n'
 
-vectorizer = CountVectorizer(min_df=3, stop_words='english')
+ps = nltk.stem.PorterStemmer()
+wnl = nltk.wordnet.WordNetLemmatizer()    
+
+ps = nltk.stem.PorterStemmer()
+wnl = nltk.wordnet.WordNetLemmatizer()    
+
+def lemmatize_sent(list_word):
+    # Text input is string, returns array of lowercased strings(words).
+    return [wnl.lemmatize(word.lower(), pos=penn2morphy(tag)) for word, tag in pos_tag(list_word)]
+
+def stem_rmv_punc(doc):
+    return (word for word in lemmatize_sent(analyzer(doc)) if word not in combined_stopwords and not word.isdigit())
+
+analyzer = text.CountVectorizer().build_analyzer()
+
+# Remove stopword, number and set min_df = 3
+vectorizer = text.CountVectorizer(min_df=3, stop_words='english', analyzer=stem_rmv_punc, token_pattern = r'(?u)\b[A-Za-z][A-Za-z]+\b')
+
+# For the train data
 X_train_counts = vectorizer.fit_transform(train_dataset.data)
 X_train_counts.toarray()
 
+# For the test data
 X_test_counts = vectorizer.transform(test_dataset.data)
 X_test_counts.toarray()
 
-from sklearn.feature_extraction.text import TfidfTransformer
-tfidf_transformer = TfidfTransformer()
-
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+# The following is the tfidf part
+# For the train data
+X_train_tfidf = text.TfidfTransformer().fit_transform(X_train_counts)
 print('Shape of TF-IDF train matrices subset: ')
 print(X_train_tfidf.shape)
-print()
 
-X_test_tfidf = tfidf_transformer.fit_transform(X_test_counts)
+# For the test data
+X_test_tfidf = text.TfidfTransformer().fit_transform(X_test_counts)
 print('Shape of TF-IDF test matrices subset: ')
 print(X_test_tfidf.shape)
 
+#%%
+"""
+QUESTION 3
+"""
+print("----------- QUESTION 3 -----------")
 
-# Add LEMMATIZATION: check doc on ccle (https://ccle.ucla.edu/mod/resource/view.php?id=2277904)
+# LSI dimensionality reduction
+
+from sklearn.decomposition import TruncatedSVD
+
+lsi = TruncatedSVD(n_components=50, random_state=42) 
+X_train_LSI = lsi.fit_transform(X_train_tfidf)
+X_test_LSI = lsi.transform(X_test_tfidf)
+print('Shape of reduce TF-IDF matrix with LSI: ')
+print('Train: ', X_train_LSI.shape)
+print('Test: ', X_test_LSI.shape)
+
+
+# NMF dimensionality reduction
+
+from sklearn.decomposition import NMF
+
+nmf = NMF(n_components=50, random_state=42)
+X_train_NMF = nmf.fit_transform(X_train_tfidf)
+X_test_NMF = nmf.transform(X_test_tfidf)
+print('Shape of reduce TF-IDF matrix with NMF: ')
+print('Train: ', X_train_NMF.shape)
+print('Test: ', X_test_NMF.shape)
+
+
+# Compare LSI & NMF
+
+# for NMF
+H = nmf.components_
+sum_train_NMF = np.sum(np.array(X_train_tfidf - X_train_NMF.dot(H))**2)
+sum_test_NMF = np.sum(np.array(X_test_tfidf - X_test_NMF.dot(H))**2)
+print(sum_train_NMF)
+print(sum_test_NMF)
+
+# for LSI
+
+
+#%%
+"""
+QUESTION 3
+"""
+print("----------- QUESTION 4 -----------")
